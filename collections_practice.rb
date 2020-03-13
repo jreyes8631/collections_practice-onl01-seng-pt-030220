b9cd78c8468f60215e6ed7de187d68bdbd1098b4
Jose E Reyes <jreyes8631@gmail.com>
2020-03-13 22:56:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@ def sort_array_asc(array)
   array.sort
 end
 
-sort_array_asc([25, 7, 1]) #[1,7,25]
+
 
 def sort_array_desc(array)
   array.sort do | left, right|
@@ -10,7 +10,6 @@ def sort_array_desc(array)
   end
 end
 
-sort_array_desc([25, 7, 14]) #[25, 14, 7]
 
 def sort_array_char_count(array)
   array.sort do |left, right|
